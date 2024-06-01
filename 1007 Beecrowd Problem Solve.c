c9130c36be95d94323147caa57246d63a6028381
Md Jakaria Nur <mdjakarianur.in@gmail.com>
2024-06-01 15:39:44 +0600

    Update 1007 Beecrowd Problem Solve.c

--- 1007 Beecrowd Problem Solve.c
+++ 1007 Beecrowd Problem Solve.c
@@ -1,1 +1,9 @@
+#include <stdio.h>
+int main() {
+int A, B, C, D;
+int difference;
+scanf("%d %d %d %d", &A, &B, &C, &D);
+difference = (A * B) - (C * D);
+printf("DIFERENCA = %d\n", difference);
+return 0; }
 
