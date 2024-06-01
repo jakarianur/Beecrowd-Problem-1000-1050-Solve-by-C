8ef8bf4a6fde18d0f30e6c4f7495e117cf70f607
Md Jakaria Nur <mdjakarianur.in@gmail.com>
2024-06-01 15:38:07 +0600

    Update 1006 Beecrowd Problem Solve.c

--- 1006 Beecrowd Problem Solve.c
+++ 1006 Beecrowd Problem Solve.c
@@ -1,1 +1,9 @@
+#include <stdio.h>
+int main() {
+double A, B, C;
+double average;
+scanf("%lf %lf %lf", &A, &B, &C);
+average = (A * 2 + B * 3 + C * 5) / (2 + 3 + 5);
+printf("MEDIA = %.1lf\n", average);
+return 0; }
 
